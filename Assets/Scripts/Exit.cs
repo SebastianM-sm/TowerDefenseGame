a951ce728d5c0779b3362a1e996a1e76463f228a
MichaelLutz1479 <michaellutz1479@gmail.com>
2026-07-20 14:24:42 -0700

    A

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -7,5 +7,10 @@ public class SceneManager : MonoBehaviour
     public void LoadScene(string sceneName) 
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName); 
-    } 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
